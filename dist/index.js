82de38d12b65a0bd10a48bed04aba8fafafb9a17
holmirr707 <tohroku707@gmail.com>
2025-04-26 18:20:13 +0900

    1.0.6

--- dist/index.js
+++ dist/index.js
@@ -9,13 +9,21 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 import { CookieJar } from "tough-cookie";
 import fetchcookie from "fetch-cookie";
+// htmlのテキストを整形して返す
 function conciseText(text) {
+    // 行ごとにリスト化
     const lines = text.trim().split(/\r?\n/);
+    // 行ごとに前後の空白を取り除く、かつ、空行を削除
     const conciseLines = lines.map(line => line.trim()).filter(line => line !== "");
+    // 改行で結合
     return conciseLines.join("\n");
 }
+// myfetchは独自のエラーを投げる
+// error.status, error.finalUrl, error.responseBodyを持つ
+// error.messageは、status, finalUrl, responseBodyを整形したものを返す
 export class MyFetchError extends Error {
     constructor(status, finalUrl, responseBody) {
+        // Errorのコンストラクタの引数は.messageになる
         super(`fetch failed...
       status: ${status}
       finalUrl: ${finalUrl}
@@ -26,24 +34,41 @@ export class MyFetchError extends Error {
         this.responseBody = responseBody;
     }
 }
+// 初期値のcookieJar, defaultHeadersを設定できる
 export default class MyFetch {
+    // constructorの引数には指定しない、空のオブジェクトどちらでも適応できる
     constructor({ cookieJar = new CookieJar(), defaultHeaders = {} } = {}) {
         this.cookieJar = cookieJar;
         this.fetchCookie = fetchcookie(fetch, this.cookieJar);
         this.defaultHeaders = defaultHeaders;
     }
+    // cookieJar, defaultHeadersを使用してfetchを行う
+    // fetch自体がエラーが発生した場合は、statusを1000に設定し独自のエラーを投げる
+    // response.okがfalseの場合も独自のエラーを投げる
     fetch(url, init) {
         return __awaiter(this, void 0, void 0, function* () {
-            const response = yield this.fetchCookie(url, Object.assign({ headers: Object.assign(Object.assign({}, this.defaultHeaders), init === null || init === void 0 ? void 0 : init.headers) }, init));
-            if (!response.ok) {
-                throw new MyFetchError(response.status, response.url, yield response.text());
+            try {
+                const response = yield this.fetchCookie(url, Object.assign({ headers: Object.assign(Object.assign({}, this.defaultHeaders), init === null || init === void 0 ? void 0 : init.headers) }, init));
+                if (!response.ok) {
+                    throw new MyFetchError(response.status, response.url, yield response.text());
+                }
+                return response;
             }
-            return response;
+            catch (e) {
+                if (e instanceof MyFetchError) {
+                    throw e;
+                }
+                throw new MyFetchError(1000, url.toString(), e.message);
+            }
         });
     }
+    // 初期値のcookieJar, defaultHeadersを設定できる
+    // 使用しやすいようにインスタンスだけでなく、関数自体も返す。
     static create({ cookieJar, defaultHeaders } = {}) {
         const myFetch = new MyFetch({ cookieJar, defaultHeaders });
         return {
+            // もしmyFetch.fetchだけを返してしまうと、thisはthisのままで[[prototype]]の関数への参照がそのまま返るだけ
+            // そのため、bindしてthisを固定して返す
             fetch: myFetch.fetch.bind(myFetch),
             client: myFetch
         };
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,26 @@ import { CookieJar } from "tough-cookie";
 import fetchcookie from "fetch-cookie";
 import { FetchCookieImpl } from "fetch-cookie";
 
+// htmlのテキストを整形して返す
 function conciseText(text: string) {
+  // 行ごとにリスト化
   const lines = text.trim().split(/\r?\n/);
-  const conciseLines = lines.map(line => line.trim()).filter(line=> line!== "");
+  // 行ごとに前後の空白を取り除く、かつ、空行を削除
+  const conciseLines = lines.map(line => line.trim()).filter(line => line !== "");
+  // 改行で結合
   return conciseLines.join("\n");
 }
 
+// myfetchは独自のエラーを投げる
+// error.status, error.finalUrl, error.responseBodyを持つ
+// error.messageは、status, finalUrl, responseBodyを整形したものを返す
 export class MyFetchError extends Error {
   status: number;
   finalUrl: string;
   responseBody: string;
 
   constructor(status: number, finalUrl: string, responseBody: string) {
+    // Errorのコンストラクタの引数は.messageになる
     super(`fetch failed...
       status: ${status}
       finalUrl: ${finalUrl}
@@ -25,28 +33,44 @@ export class MyFetchError extends Error {
   }
 }
 
+// 初期値のcookieJar, defaultHeadersを設定できる
 export default class MyFetch {
   public cookieJar: CookieJar;
   public fetchCookie: FetchCookieImpl<RequestInfo | URL, RequestInit, Response>;
   public defaultHeaders: RequestInit["headers"];
 
-  constructor({cookieJar=new CookieJar(), defaultHeaders={}}: {cookieJar?: CookieJar, defaultHeaders?: RequestInit["headers"]} = {}) {
+  // constructorの引数には指定しない、空のオブジェクトどちらでも適応できる
+  constructor({ cookieJar = new CookieJar(), defaultHeaders = {} }: { cookieJar?: CookieJar, defaultHeaders?: RequestInit["headers"] } = {}) {
     this.cookieJar = cookieJar;
     this.fetchCookie = fetchcookie(fetch, this.cookieJar);
     this.defaultHeaders = defaultHeaders;
   }
 
+  // cookieJar, defaultHeadersを使用してfetchを行う
+  // fetch自体がエラーが発生した場合は、statusを1000に設定し独自のエラーを投げる
+  // response.okがfalseの場合も独自のエラーを投げる
   public async fetch(url: RequestInfo | URL, init?: RequestInit) {
-    const response = await this.fetchCookie(url, {headers:{...this.defaultHeaders, ...init?.headers}, ...init});
-    if (!response.ok) {
-      throw new MyFetchError(response.status, response.url, await response.text());
+    try {
+      const response = await this.fetchCookie(url, { headers: { ...this.defaultHeaders, ...init?.headers }, ...init });
+      if (!response.ok) {
+        throw new MyFetchError(response.status, response.url, await response.text());
+      }
+      return response;
+    } catch (e) {
+      if (e instanceof MyFetchError) {
+        throw e;
+      }
+      throw new MyFetchError(1000, url.toString(), (e as Error).message);
     }
-    return response;
   }
 
-  static create({cookieJar, defaultHeaders}: {cookieJar?: CookieJar, defaultHeaders?: RequestInit["headers"]} = {}) {
-    const myFetch = new MyFetch({cookieJar, defaultHeaders});
+  // 初期値のcookieJar, defaultHeadersを設定できる
+  // 使用しやすいようにインスタンスだけでなく、関数自体も返す。
+  static create({ cookieJar, defaultHeaders }: { cookieJar?: CookieJar, defaultHeaders?: RequestInit["headers"] } = {}) {
+    const myFetch = new MyFetch({ cookieJar, defaultHeaders });
     return {
+      // もしmyFetch.fetchだけを返してしまうと、thisはthisのままで[[prototype]]の関数への参照がそのまま返るだけ
+      // そのため、bindしてthisを固定して返す
       fetch: myFetch.fetch.bind(myFetch) as typeof fetch,
       client: myFetch
     }
